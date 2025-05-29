62865abd8cbf00d1322d78f0ae1f8c63bdc58030
Srimathy <ssrimathy284@.com>
2025-05-29 14:22:39 +0530

    add task functionality

--- ToDo_main/settings.py
+++ ToDo_main/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ToDo_main',  # Custom app for the ToDo application
+    'ToDo_main', 
+     'todo' ,
 ]
 
 MIDDLEWARE = [
---
+++ todo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import redirect, render
+from .models import Task
+
+
+# Create your views here.
+def addTask(request):
+    task = request.POST.get('task')
+    Task.objects.create(task=task)
+    return redirect('home')
